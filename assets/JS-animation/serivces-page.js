470808dd470606123e1dd0f997b07f062aee6397
Naresh Narnapati <naresh.narnapati@reymould.com>
2025-10-30 17:10:36 +0530

    Service page changes added

---
+++ assets/JS-animation/serivces-page.js
@@ -0,0 +1,6 @@
+document.addEventListener("scroll", () => {
+    document.querySelectorAll(".fade-in").forEach((el) => {
+        const rect = el.getBoundingClientRect();
+        if (rect.top < window.innerHeight - 100) el.classList.add("show");
+    });
+});
